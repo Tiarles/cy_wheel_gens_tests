8a6c954d46e95469767638655aa29a5da5e5cddb
Tiarles Moralles Guterres <tiarles.moralles@typhoon-hil.com>
2024-03-22 13:24:52 +0100

    setup.py updates

--- setup.py
+++ setup.py
@@ -5,24 +5,30 @@ import shutil
 FOLDER_DIR_PATH = os.path.dirname(__file__)
 sys.path.insert(0, FOLDER_DIR_PATH)
 
-shutil.copy(R"C:\WORK\generators_test_pys\main.py", R"C:\WORK\generators_test_pys\src\test_gens\impl.pyx")
+ext_modules = None
 
-from Cython.Build import cythonize
-from Cython.Compiler.Errors import CompileError
-try:
-    language_level = 3
-    ext_modules = cythonize(
-        [R"src\test_gens\impl.pyx"],
-        compiler_directives={
-            "binding": True,  # so inspect function works properly on cythonized functions
-            "language_level": language_level,
-        },  # avoid cython warnings
+if "bdist_wheel" in sys.argv:
+    shutil.copy(
+        src=os.path.join(FOLDER_DIR_PATH, "main.py"), 
+        dst=os.path.join(FOLDER_DIR_PATH, R"src\test_gens\impl.pyx")
     )
-except CompileError:
-    import sys
-    sys.exit()
 
-os.remove(R"C:\WORK\generators_test_pys\src\test_gens\impl.pyx")
+    from Cython.Build import cythonize
+    from Cython.Compiler.Errors import CompileError
+    try:
+        language_level = 3
+        ext_modules = cythonize(
+            [R"src\test_gens\impl.pyx"],
+            compiler_directives={
+                "binding": True,  # so inspect function works properly on cythonized functions
+                "language_level": language_level,
+            },  # avoid cython warnings
+        )
+    except CompileError:
+        import sys
+        sys.exit()
+
+    os.remove(os.path.join(FOLDER_DIR_PATH, R"src\test_gens\impl.pyx"))
 
 setup(
     ext_modules=ext_modules
