4f156277018352103e3cde8eb89d48467905edd4
Tiarles Moralles Guterres <tiarles.moralles@typhoon-hil.com>
2024-03-25 11:24:09 +0100

    Reverting to make the example simple

--- plugin.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtestloop(session):
-    print("Before the pytest_runtestloop loop.")
-    yield
-    print("After the pytest_runtestloop loop.")
--- setup.py
+++ setup.py
@@ -8,44 +8,27 @@ sys.path.insert(0, FOLDER_DIR_PATH)
 ext_modules = None
 
 if "bdist_wheel" in sys.argv:
-    try:
-        os.remove(os.path.join(FOLDER_DIR_PATH, R"src\test_gens\pytest_hooks_gens\plugin.py"))
-    except FileNotFoundError:
-        pass
-
-    for src_rel, dst_rel in [ 
-        ("main.py", R"src\test_gens\impl.pyx"),
-        ("plugin.py", R"src\test_gens\pytest_hooks_gens\plugin.pyx")
-    ]:
-        shutil.copy(
-            src=os.path.join(FOLDER_DIR_PATH, src_rel),
-            dst=os.path.join(FOLDER_DIR_PATH, dst_rel)
-        )
+    shutil.copy(
+        src=os.path.join(FOLDER_DIR_PATH, "main.py"),
+        dst=os.path.join(FOLDER_DIR_PATH, R"src\test_gens\impl.pyx")
+    )
 
     from Cython.Build import cythonize
     from Cython.Compiler.Errors import CompileError
     try:
         language_level = 3
         ext_modules = cythonize(
-            [
-                R"src\test_gens\impl.pyx",
-                R"src\test_gens\pytest_hooks_gens\plugin.pyx"
-            ],
+            [R"src\test_gens\impl.pyx"],
             compiler_directives={
                 "binding": True,  # so inspect function works properly on cythonized functions
                 "language_level": language_level,
             },  # avoid cython warnings
         )
-        print(f"ext_modules: {ext_modules}")
     except CompileError:
         import sys
         sys.exit()
 
-    for pyx_file in [
-        R"src\test_gens\impl.pyx",
-        R"src\test_gens\pytest_hooks_gens\plugin.pyx"
-    ]:
-        os.remove(os.path.join(FOLDER_DIR_PATH, pyx_file))
+    os.remove(os.path.join(FOLDER_DIR_PATH, R"src\test_gens\impl.pyx"))
 
 setup(
     ext_modules=ext_modules
